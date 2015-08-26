f2babbb1c5d435ec1b9aeef63c6edce5bdce3d12
Sabiha Ozturk <sabihay@hotmail.com>
2015-08-26 13:19:01 -0400

    Fixed pager about first&last recipe to show

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -10,13 +10,15 @@ class RecipesController < ApplicationController
 
   def show
     @recipe = Recipe.find params[:id]
+    @categories = Category.all
+    
     @previous_recipe = Recipe.where('created_at < ?', @recipe.created_at).
                               order(created_at: :desc).
-                              limit(1).first
+                              limit(1).first 
+  
     @next_recipe = Recipe.where('created_at > ?', @recipe.created_at).
                               order(created_at: :asc).
                               limit(1).first
-    @categories = Category.all
   end
 
   def list
